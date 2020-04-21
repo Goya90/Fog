11eae3da96d619d41c30524151b1096e69724c0a
jbndk <j.b-n@live.dk>
2020-04-21 14:35:22 +0200

    Det vi lavede sammen på Zoom d. 21. april 2020

--- src/main/java/FunctionLayer/Calculator.java
+++ src/main/java/FunctionLayer/Calculator.java
@@ -1,5 +1,7 @@
 package FunctionLayer;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
     private int width;
@@ -8,10 +10,49 @@ public class Calculator {
     private boolean flat;
     private boolean shed;
 
-    private int MaterialID1Length;
-    private int MaterialID1Quantity;
+    int carportWidth = width;
+    int carportLength = length;
+    int carportHeight = height;
 
+    ArrayList<Material> bom = new ArrayList<>();
 
+public ArrayList<Material> bomCalculator (int width, int length, int height) throws LoginSampleException, ClassNotFoundException {
 
+    if (width == 0 || length == 0 || height == 0) {
+        //returner fejlbesked
+    }
+
+    //Tilføjer materiale ID 1:
+    addMaterial1();
+
+    return bom;
+}
+
+//Materiale 1 bliver tilføjet:
+public void addMaterial1 () throws LoginSampleException, ClassNotFoundException {
+    int materialId = 1;
+    int fixedQuantity = 2;
+    int minimumLength = 600;
+
+    Material mat = LogicFacade.showMaterial(materialId);
+
+    mat.setLength(minimumLength + carportLength);
+    mat.setQuantity(fixedQuantity);
+
+    bom.add(mat);
+}
+
+    //Materiale 2 bliver tilføjet:
+    public void addMaterial2 () throws LoginSampleException, ClassNotFoundException {
+        int materialId = 2;
+        int fixedQuantity = 1;
+
+        Material mat = LogicFacade.showMaterial(materialId);
+
+        mat.setLength(carportWidth);
+        mat.setQuantity(fixedQuantity);
+
+        bom.add(mat);
+    }
 
 }
