abe727855be63c281fa9cf15e67b7d8a46a2e354
jbndk <j.b-n@live.dk>
2020-04-30 08:50:29 +0200

    Automatisk generering af spær og stolper

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -41,7 +41,7 @@ public class Connector {
             // Localhost
             URL = "jdbc:mysql://localhost:3306/fog?serverTimezone=CET&useSSL=false";
             USERNAME = "root";
-            PASSWORD = "Rufbtr11@";
+            PASSWORD = "mario";
         }
     }
 }
--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -17,11 +17,67 @@ public class Drawing extends Command {
         /*request.getServletContext().setAttribute("width", width);
         request.getServletContext().setAttribute("length", length);
 */
-        Svg svg = new Svg(400, 300, "0,0,800,600",0,0);
-        Svg svgInnerDrawing = new Svg(900,800,"0,0,900,800",0,0);
+        Svg svg = new Svg(width, length, "0,0,800,600",0,0);
         svg.addRect(0,0,length,width);
-        svg.addRect(0,35,4,width);
-        svg.addRect(0,565,4,width);
+
+        //Variable til for loop til spær:
+        int x = 0;
+        int y = 55;
+        int thickness = 4;
+        int distanceBetweenEach = 55;
+
+        //For loop som tilføjer antal spær tilpasset carport dimensionerne:
+        for (int i = length; i > y; y = y + distanceBetweenEach) {
+          svg.addRect(x, y, thickness, width );
+        }
+
+        //Tilføjer stolper (4 stk. hvis carport længde < 480 cm. Ellers 6 stk):
+        int poleThickness = 8;
+        if (length < 481) {
+
+            int yPole1 = (length / 10) * 2;
+            int xPole1 = 30;
+            svg.addRect(xPole1, yPole1, poleThickness,poleThickness);
+
+            int yPole2 = (length / 10) * 2;
+            int xPole2 = width - 30;
+            svg.addRect(xPole2, yPole2, poleThickness,poleThickness);
+
+            int yPole3 = (length / 10) * 8;
+            int xPole3 = 30;
+            svg.addRect(xPole3, yPole3, poleThickness,poleThickness);
+
+            int yPole4 = (length / 10) * 8;
+            int xPole4 = width - 30;
+            svg.addRect(xPole4, yPole4, poleThickness,poleThickness);
+
+        } else {
+
+                int yPole1 = (length / 10) * 2;
+                int xPole1 = 30;
+                svg.addRect(xPole1, yPole1, poleThickness,poleThickness);
+
+                int yPole2 = (length / 10) * 2;
+                int xPole2 = width - 30;
+                svg.addRect(xPole2, yPole2, poleThickness,poleThickness);
+
+                int yPole3 = (length / 10) * 8;
+                int xPole3 = 30;
+                svg.addRect(xPole3, yPole3, poleThickness,poleThickness);
+
+                int yPole4 = (length / 10) * 8;
+                int xPole4 = width - 30;
+                svg.addRect(xPole4, yPole4, poleThickness,poleThickness);
+
+                int yPole5 = (length / 10) * 5;
+                int xPole5 = 30;
+                svg.addRect(xPole5, yPole5, poleThickness,poleThickness);
+
+                int yPole6 = (length / 10) * 5;
+                int xPole6 = width - 30;
+                svg.addRect(xPole6, yPole6, poleThickness,poleThickness);
+            }
+
 
         request.setAttribute("svgdrawing", svg.toString());
 
