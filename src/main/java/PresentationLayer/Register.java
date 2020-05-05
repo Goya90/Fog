2ed4d426ac4909403d3d5b77f4ccd0289a5ad3e7
Claes <claesvonh@gmail.com>
2020-05-05 16:39:14 +0200

    Oprettelse af ny bruger tilpasset bruger attributter

--- src/main/java/PresentationLayer/Register.java
+++ src/main/java/PresentationLayer/Register.java
@@ -20,8 +20,7 @@ public class Register extends Command {
 
             session.setAttribute("email",email);
             session.setAttribute( "user", user );
-            session.setAttribute( "role", user.getRole() );
-            return user.getRole() + "page";
+            return "employeepage";
         } else {
             throw new LoginSampleException( "the two passwords did not match" );
         }
